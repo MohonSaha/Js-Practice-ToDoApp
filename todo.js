b99c7f734379bd867db51bf91aec477f236ebe1c
MohonSaha <practicewithmohon@gmail.com>
2023-02-16 14:36:16 +0600

    play with event

--- todo.js
+++ todo.js
@@ -10,7 +10,7 @@ document.getElementById('input-btn').addEventListener("click", function(){
                    <td>${myInputValue}</td>
                    <td> 
                    <button class="btn btn-danger delete-btn">Delete</button>
-                   <button class="btn btn-success done-btn">Done</button>
+                   <button class="btn btn-primary done-btn">Done</button>
                    </td>
 
   `;
@@ -24,7 +24,6 @@ else{
 
 
 
-
 const elementsDel = document.querySelectorAll('.delete-btn');
 for(const value of elementsDel){
   /* By this event (object) we can know everything about the element 
@@ -34,7 +33,36 @@ for(const value of elementsDel){
   })
 }
 
+// const elementsDel = document.querySelectorAll('.delete-btn');
+// for(const value of elementsDel){
+//   /* By this event (object) we can know everything about the element 
+//   that is clicked amoung the others element. We can use any name instade of event as the parameter. We can know it's html, it's parent. it's child, it's grandfather ect. Besides we can access all the things like object property. */
+//   value.addEventListener('click', function(event){
+//     event.target.parentNode.parentNode.style.display = "none";
+//   })
+// }
+
+
+
+document.getElementById('clear-btn').addEventListener('click', function(e){
+  
+  // console.log(e.target.previousSibling);
+  e.target.parentNode.style.display = "none";
+})
+
+
+
+
+
+
+
+
+
+
 
 })
+
+
+
 
 
